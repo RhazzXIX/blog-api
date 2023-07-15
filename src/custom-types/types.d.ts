7ba56063873644189126203f029d8f4ff0034f50
RhazzXIX <jm21tonio@gmail.com>
2023-07-15 15:58:05 +0800

    Add custom type post and declare types globally

--- src/custom-types/types.d.ts
+++ src/custom-types/types.d.ts
@@ -1,6 +1,21 @@
-interface IUser {
-  name: string,
-  password: string,
-  isAuthor?: boolean,
-  email: string,
+import { Types } from "mongoose";
+
+declare global {
+  interface IUser {
+    name: string;
+    password: string;
+    isAuthor?: boolean;
+    email: string;
+  }
+}
+
+declare global {
+  interface IPost {
+    title: string;
+    text: string;
+    date: Date;
+    isPublished: boolean;
+    publishedDate?: Date;
+    author: Types.ObjectId;
+  }
 }
