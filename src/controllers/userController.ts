85ab08eea1aff8ede008c01eb567740c5ec10321
RhazzXIX <jm21tonio@gmail.com>
2023-07-18 19:10:25 +0800

    Create middleware & API for user log in POST req

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,6 +2,8 @@ import asyncHandler from "express-async-handler";
 import User from "../models/user";
 import { ValidationError, body, validationResult } from "express-validator";
 import bcrypt from "bcryptjs";
+import passport, { AuthenticateCallback } from "passport";
+import { Handler } from "express";
 
 const userController = {
   userSignUp: [
@@ -30,17 +32,15 @@ const userController = {
 
       // Errors from validation.
       const validationErrors = validationResult(req);
-      // Add validation errors to container.
 
+      // Add validation errors to container.
       if (!validationErrors.isEmpty())
         validationErrors.array().forEach((err) => errors.push(err));
       // If user already used, add to error container.
-
       const userExists = await User.findOne({ name: username }).exec();
       if (userExists) errors.push({ msg: "User already exists." });
       if (errors.length > 0) {
         // Send validation errors
-        console.log(req.body);
         res.status(400).json(errors);
       } else {
         bcrypt.hash(password, 10, async function (err, hashedPassword) {
@@ -66,6 +66,48 @@ const userController = {
         });
       }
     }),
+  ],
+  userLogin: [
+    // Validate and sanitize user input
+    body("username", "Username should have at least 3 characters.")
+      .trim()
+      .isLength({ min: 3 })
+      .escape(),
+    body("password", "Password should have at least 5 characters.")
+      .trim()
+      .isLength({ min: 5 })
+      .escape(),
+    asyncHandler(async function (req, res, next) {
+      const errors: { error: { msg: string } }[] = [];
+
+      // Check for validation errors.
+      const validationErrors = validationResult(req);
+
+      // Authenticate user if there are no validation errors.
+      if (validationErrors.isEmpty()) {
+        await passport.authenticate("local", {}, function (err, user, info) {
+          if (err) return next(err);
+          if (info) return res.json(info);
+          if (user) {
+            req.login(user, function (err) {
+              if (err) return next(err);
+              next();
+            });
+          }
+        } as AuthenticateCallback)(req, res, next);
+      } else {
+        // Send errors.
+        validationErrors.array().forEach((err) => {
+          const { msg } = err;
+          errors.push({ error: { msg } });
+        });
+        res.status(400).json(errors);
+      }
+    }),
+    function (req, res, next) {
+      if (req.user)
+        res.status(200).json({ message: `User ${req.user.name} Logged in` });
+    } as Handler,
   ],
 };
 
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,8 +1,11 @@
-import express from 'express'
-import userController from '../controllers/userController';
+import express from "express";
+import userController from "../controllers/userController";
 const router = express.Router();
 
-// Route for user sign-up.
-router.post('/sign-up', userController.userSignUp);
+// API for user sign-up.
+router.post("/sign-up", userController.userSignUp);
+
+// API for user log in.
+router.post("/log-in", userController.userLogin);
 
 export default router;
