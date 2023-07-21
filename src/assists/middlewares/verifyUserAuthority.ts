d591f91f7d9e55be5bf7477654ba719e40b73723
RhazzXIX <jm21tonio@gmail.com>
2023-07-21 09:36:03 +0800

    Add verifyUserAuthority helper middleware
    Added verifyUserAuthority middleware for verifying users it they can
    access unpublished posts.

---
+++ src/assists/middlewares/verifyUserAuthority.ts
@@ -0,0 +1,47 @@
+import { Handler } from "express";
+import Post from "../../models/post";
+import asyncHandler from "express-async-handler";
+import mongoose from "mongoose";
+
+const verifyUserAuthority = asyncHandler(async function (req, res, next) {
+  // Get post id.
+  const { postId } = req.params;
+  // check if valid req.
+  if (mongoose.isValidObjectId(postId) === false)
+    return res.status(404).json({ message: "Post not found" });
+  // Fetch post.
+  const post = await Post.findById(postId).exec();
+  // Check if post is found.
+  if (post) {
+    // Check if post is published
+    if (post.isPublished) {
+      // Send post to next middleware.
+      res.locals.post = await post.populate({
+        path: "author",
+        select: "name",
+        options: { lean: true },
+      });
+      // Proceed to next middleware.
+      next();
+    } else {
+      // If the post is unpublished, check user if he is an Author.
+      if (req.user && req.user.isAuthor) {
+        // Send post to next middleware.
+        res.locals.post = await post.populate({
+          path: "author",
+          select: "name",
+          options: { lean: true },
+        });
+        // Proceed to next middleware.
+        next();
+      } else {
+        // Send unauthorized error.
+        res.status(401).json({ message: "Unauthorized request" });
+      }
+    }
+  } else {
+    res.status(404).json({ message: "Post not found" });
+  }
+} as Handler);
+
+export default verifyUserAuthority;
--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -4,6 +4,7 @@ import asyncHandler from "express-async-handler";
 import { body, validationResult, ValidationError } from "express-validator";
 import { Handler } from "express";
 import mongoose from "mongoose";
+import verifyUserAuthority from "../assists/middlewares/verifyUserAuthority";
 
 const postController = {
   // Middleware for getting all blog posts.
@@ -29,48 +30,19 @@ const postController = {
       // Send posts data.
       res.status(200).json(posts);
     } else {
-      res.status(204).json({ message: "There are no posts yet" });
+      res.status(404).json({ message: "There are no posts yet." });
     }
   }),
 
   // Middleware for getting a single post.
-  blogPost: asyncHandler(async function (req, res, next) {
-    // Blog Post that user requests.
-    const { postId } = req.params;
-
-    // Check if a valid request.
-    if (mongoose.isValidObjectId(postId)) {
-      // Fetched blog post data
-
-      // Fetch the post
-      const post = await Post.findById(postId)
-        .populate({ path: "author", select: "name" })
-        .lean()
-        .exec();
-
-      // Check if the was post valid and published.
-      if (post && post.isPublished) {
-        // Send data if the post is published.
-        res.status(200).json(post);
-
-        // If post is not published, check if the user is the author.
-      } else if (post && post.isPublished === false) {
-        if (req.user && req.user.isAuthor) {
-          // Send the unpublished post
-          res.status(200).json(post);
-        } else {
-          // Send unauthorized if the user is not an author.
-          res.status(401).json({ message: "Unauthorized" });
-        }
-      } else {
-        // Else send not found error.
-        res.status(400).json({ message: "Post not found" });
-      }
-    } else {
-      // Send error if invalid request.
-      res.status(400).json({ message: "Post not found" });
-    }
-  }),
+  blogPost: [
+    verifyUserAuthority,
+    asyncHandler(async function (req, res, next) {
+      const post = res.locals.post;
+      // Send post.
+      res.status(200).json(post);
+    }),
+  ],
 };
 
 export default postController;
